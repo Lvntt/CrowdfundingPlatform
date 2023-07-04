55d90b5eb02742353812bf366edca0384452fb4d
Matvey <53006125+Lvntt@users.noreply.github.com>
2023-07-04 16:36:08 +0700

    added Ubuntu font and some packages

---
+++ app/src/main/java/com/example/crowdfundingplatform/presentation/ui/screen/RegistrationCredentialsScreen.kt
@@ -0,0 +1,11 @@
+package com.example.crowdfundingplatform.presentation.ui.screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RegistrationCredentialsScreen(
+    modifier: Modifier = Modifier
+) {
+
+}

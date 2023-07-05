a5b03032371be4f725030e0885e9376396364581
Matvey <53006125+Lvntt@users.noreply.github.com>
2023-07-05 23:06:04 +0700

    Fix navigation

--- app/src/main/java/com/example/crowdfundingplatform/presentation/ui/navigation/CrowdfundingNavGraph.kt
+++ app/src/main/java/com/example/crowdfundingplatform/presentation/ui/navigation/CrowdfundingNavGraph.kt
@@ -1,11 +1,13 @@
 package com.example.crowdfundingplatform.presentation.ui.navigation
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.crowdfundingplatform.presentation.ui.screen.LoginScreen
 import com.example.crowdfundingplatform.presentation.ui.screen.OnboardingScreen
+import com.example.crowdfundingplatform.presentation.ui.screen.ProfileInfoScreen
 import com.example.crowdfundingplatform.presentation.ui.screen.RegistrationCredentialsScreen
 import com.example.crowdfundingplatform.presentation.ui.screen.RegistrationPersonalInfoScreen
 
@@ -34,23 +36,37 @@ fun Navigation(
             LoginScreen(
                 onSignInClick = { navController.navigate(CrowdfundingDestinations.PROFILE_INFO_ROUTE) },
                 onSignUpClick = { navController.navigate(CrowdfundingDestinations.REGISTRATION_PERSONAL_INFO_ROUTE) },
-                onNavigateUp = { navController.popBackStack() }
+                onNavigateUp = { navController.navigate(CrowdfundingDestinations.ONBOARDING_ROUTE) }
             )
+            BackHandler {
+                navController.navigate(CrowdfundingDestinations.ONBOARDING_ROUTE)
+            }
         }
         composable(CrowdfundingDestinations.REGISTRATION_PERSONAL_INFO_ROUTE) {
             RegistrationPersonalInfoScreen(
                 onContinueClick = { navController.navigate(CrowdfundingDestinations.REGISTRATION_CREDENTIALS_ROUTE) },
-                onNavigateUp = { navController.popBackStack() }
+                onNavigateUp = {
+                    navController.navigate(CrowdfundingDestinations.LOGIN_ROUTE)
+                }
             )
+            BackHandler {
+                navController.navigate(CrowdfundingDestinations.LOGIN_ROUTE)
+            }
         }
         composable(CrowdfundingDestinations.REGISTRATION_CREDENTIALS_ROUTE) {
             RegistrationCredentialsScreen(
                 onSignUpClick = { navController.navigate(CrowdfundingDestinations.PROFILE_INFO_ROUTE) },
                 onNavigateUp = { navController.navigate(CrowdfundingDestinations.LOGIN_ROUTE) }
             )
+            BackHandler {
+                navController.navigate(CrowdfundingDestinations.LOGIN_ROUTE)
+            }
         }
         composable(CrowdfundingDestinations.PROFILE_INFO_ROUTE) {
-            // TODO
+            ProfileInfoScreen(
+                name = "Placeholder name",
+                email = "placeholder@xddddd.com"
+            )
         }
     }
 }

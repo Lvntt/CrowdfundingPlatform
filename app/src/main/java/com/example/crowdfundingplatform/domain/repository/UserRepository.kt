c7c6c3ee962931a8004d6628b29b5b308be6cbb1
Suslanium <zaharov22m@gmail.com>
2023-07-12 22:14:47 +0700

    Add edit profile method and use case with api support

--- app/src/main/java/com/example/crowdfundingplatform/domain/repository/UserRepository.kt
+++ app/src/main/java/com/example/crowdfundingplatform/domain/repository/UserRepository.kt
@@ -1,9 +1,12 @@
 package com.example.crowdfundingplatform.domain.repository
 
+import com.example.crowdfundingplatform.domain.entity.EditProfileRequest
 import com.example.crowdfundingplatform.domain.entity.User
 
 interface UserRepository {
 
     suspend fun getYourProfile(): User
+
+    suspend fun editYourProfile(editProfileRequest: EditProfileRequest): User
 
 }

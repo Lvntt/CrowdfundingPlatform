69fbe10113372d031185f1f0ac4ee323071357c2
Suslanium <zaharov22m@gmail.com>
2023-07-14 02:05:53 +0700

    Add navigation to project info screen

--- app/src/main/java/com/example/crowdfundingplatform/presentation/ui/extension/Extension.kt
+++ app/src/main/java/com/example/crowdfundingplatform/presentation/ui/extension/Extension.kt
@@ -5,10 +5,21 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.navigation.NavDestination
 
 fun Modifier.noRippleClickable(onClick: () -> Unit): Modifier = composed {
-    clickable(indication = null,
-        interactionSource = remember { MutableInteractionSource() }) {
+    clickable(indication = null, interactionSource = remember { MutableInteractionSource() }) {
         onClick()
+    }
+}
+
+fun NavDestination.routeWithoutArguments(): String? {
+    return if (route?.contains("/") == true) {
+        val argumentSeparatorIndex = route?.indexOf('/')
+        if (argumentSeparatorIndex != null && argumentSeparatorIndex != -1) route?.substring(
+            0, argumentSeparatorIndex
+        ) else null
+    } else {
+        route
     }
 }
---
+++ app/src/main/java/com/example/crowdfundingplatform/presentation/ui/screen/ProjectInfoScreen.kt
@@ -0,0 +1,10 @@
+package com.example.crowdfundingplatform.presentation.ui.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProjectInfoScreen(
+    projectId: String
+) {
+
+}

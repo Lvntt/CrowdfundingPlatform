f8db4b7ddca8a0feb0a9f68ed4a5f2129b4c51a0
Lantt <daymatt@mail.ru>
2023-07-13 17:50:26 +0700

    Add data layer files for project creation

--- app/src/main/java/com/example/crowdfundingplatform/data/repository/ProjectRepositoryImpl.kt
+++ app/src/main/java/com/example/crowdfundingplatform/data/repository/ProjectRepositoryImpl.kt
@@ -1,16 +1,25 @@
 package com.example.crowdfundingplatform.data.repository
 
+import com.example.crowdfundingplatform.data.datasource.TokenDataSource
 import com.example.crowdfundingplatform.data.remote.api.ProjectApiService
+import com.example.crowdfundingplatform.data.remote.model.TokenType
+import com.example.crowdfundingplatform.domain.entity.ProjectCreationRequest
 import com.example.crowdfundingplatform.domain.entity.SearchProjectsRequest
 import com.example.crowdfundingplatform.domain.entity.SearchProjectsResponse
 import com.example.crowdfundingplatform.domain.repository.ProjectRepository
 
 class ProjectRepositoryImpl(
-    private val projectApiService: ProjectApiService
+    private val projectApiService: ProjectApiService,
+    private val tokenDataSource: TokenDataSource
 ) : ProjectRepository {
 
     override suspend fun getAllProjects(body: SearchProjectsRequest): SearchProjectsResponse {
        return projectApiService.searchProjects(body)
+    }
+
+    override suspend fun createProject(body: ProjectCreationRequest) {
+        val accessToken = tokenDataSource.fetchToken(TokenType.ACCESS)!!
+        return projectApiService.createProject("Bearer $accessToken", body)
     }
 
 }
